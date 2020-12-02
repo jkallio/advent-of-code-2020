0a3fc487b267afd41f90070e76687f926dfe19ac
Jussi Kallio <jkallio@gmail.com>
2020-12-02 22:03:44 +0200

    add parsing for min and max values

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
+use std::io::{BufReader, BufRead, Error, ErrorKind};
 
 // Split file into vector of strings
 fn split_file(path: &str) -> Result<Vec<String>, std::io::Error> {    
@@ -16,18 +15,42 @@ fn split_file(path: &str) -> Result<Vec<String>, std::io::Error> {
     return Ok(vec);
 }
 
+// Returns parsed min and max values from input string (e.g. "1-7")
+fn parse_min_max(range: &str) -> Result<(i32, i32), std::io::Error> {
+    let mut it = range.split("-");
+    let a = it.next().unwrap().parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+    let b = it.next().unwrap().parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+    return Ok((a,b));
+}
+
 // Main function
 fn main() {
     let filename = "input.txt";
     let result = split_file(filename);
     match result {
         Ok(v) => {
+            // Loop through the stored passwords
             for x in v {
-                println!("{}", x)
+                // Split the line => min-max; character; password
+                let mut token_iter = x.split_whitespace();
+                if let Ok((a, b)) = parse_min_max(token_iter.next().unwrap()) {
+                    println!("min={}; max={}", a, b);
+                }
+                
+                /*
+                let tokens:Vec<&str> = x.split_whitespace().collect();
+                let range = parse_range(tokens[0]);
+                */
             }
         },
         Err(e) => {
             println!("*** Error: {}", e);
         }
     }
+
+/*
+18-19 p: fvpkgfkfjgwllqwhrjd
+12-13 v: kvvvbmdvvvvvvcvvvv
+3-5 m: mdmkmvhszpjcxl
+*/
 }
