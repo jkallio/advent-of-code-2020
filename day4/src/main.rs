8ba12c2d9b95ac2833684bebd7a46c5e6732d7a9
Jussi Kallio <jkallio@gmail.com>
2020-12-05 01:18:18 +0200

    Found solution for part-2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,26 +2,127 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 mod passport;
 use passport::Passport;
-//use passport::Validator;
+use regex::Regex;
 
+// Parse 32-bit integer from string
+//  + value:    Input value as string
+// -> Returns i32 if input is valid integer; None otherwise
+fn parse_i32(value: &str) -> Option<i32> {
+    let i = value.parse::<i32>();
+    match i {
+        Ok(val) => Some(val),
+        Err(_e) => None
+    }
+}
+
+// Parse (Height) - a number followed by either cm or in: cm[150, 193]; in[59, 67]
+//  + value:    Input height as string
+// -> Returns i32 if input is valid height; None otherwise
+fn parse_height(value:&str) -> Option<i32> {
+    if value.contains("in") {
+        if let Some(h) = parse_i32(&value[0..2]) {
+            if h >= 59 && h <= 76 {
+                return Some(h);
+            }
+        }
+    }
+    else if value.contains("cm") {
+        if let Some(h) = parse_i32(&value[0..3]) {
+            if h >= 150 && h <= 193 {
+                return Some(h);
+            }
+        }
+    }
+    None
+}
+
+// Parse (Year) - four digits
+//  + value:    Input year as string
+//  + at_least: A valid year must be at least given value
+//  + at_most:  A valid year must not be over given value
+//  -> Returns the year if input is vald integer; None otherwise
+fn parse_year(value:&str, at_least:i32, at_most:i32) -> Option<i32> {
+    if let Some(y) = parse_i32(value) {
+        if y >= at_least && y <= at_most {
+            return Some(y);
+        }
+    }
+    None
+}
+
+// Parse (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+//  + value:    Input string
+//  -> Returns the string if valid #rrggbb; None otherwise
+fn parse_hair_color(value: &str) -> Option<String> {
+    let re = Regex::new(r"#[a-f,0-9]{6}").unwrap();
+    if re.is_match(value) {
+        return Some(String::from(value));
+    }
+    None
+}
+
+// Parse (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+//  + value:    Input string
+//  -> Returns the string if matches determined set of values; None otherwise
+fn parse_eye_color(value: &str) -> Option<String> {
+    if value == "amb" || value == "blu" || value == "brn" || value == "gry" || value == "grn" || value == "hzl" || value == "oth" {
+        return Some(String::from(value));
+    }
+    else {
+        return None;
+    }
+}
+
+// Parse (Passport ID) - a nine-digit number, including leading zeroes.
+//  + value:    Input string
+//  -> Returns passport ID as integer if valid; None otherwise
+fn parse_passport_id(value: &str) -> Option<i32> {
+    let re = Regex::new(r"\b[0-9]{9}\b").unwrap();
+    if re.is_match(value) {
+        return parse_i32(value);
+    }
+    else {
+        return None;
+    }
+}
+
+// Parse input string into a Passport struct
+//  + line:     Input string to be parsed
+//  -> Returns Passport struct
 fn parse_line(line: &str) -> Passport {
     let mut pass = Passport::new();
     let params = line.split_whitespace();
     for it in params {
-        pass.set(it);
+        let mut parts = it.split(":");
+        let key = parts.next().unwrap();
+        let value = parts.next().unwrap();
+        match key {
+            "byr" => pass.byr = parse_year(value, 1920, 2002),
+            "iyr" => pass.iyr = parse_year(value, 2010, 2020),
+            "eyr" => pass.eyr = parse_year(value, 2020, 2030),
+            "hgt" => pass.hgt = parse_height(value),
+            "hcl" => pass.hcl = parse_hair_color(value),
+            "ecl" => pass.ecl = parse_eye_color(value),
+            "pid" => pass.pid = parse_passport_id(value),
+            "cid" => pass.cid = parse_i32(value),
+            _ => panic!("Unknwon param name!")
+        }
     }
     return pass;
 }
 
-
+// Parse passport file 
+// - Passport fields are separated by whitespace (space / linebreak)
+// - Passports are separated by empty line
+//  + path:     Path to the input file
+//  -> Returns list of Passports; Error in case of error
 fn parse_file(path: &str) -> Result<Vec<Passport>, std::io::Error> {
-    let file = File::open(path)?;
+    let file = File::open(path)?;   // Early exit in case of IO error
     let br = BufReader::new(file);
-
     let mut vec = Vec::new();
     let mut pass = Passport::new();
     for line in br.lines() {
-        let line = line?;
+        let line = line?; // Early exit in case of IO error
 
         // Create new passport (separated by blank line)
         if line.len() == 0 {
@@ -33,11 +134,8 @@ fn parse_file(path: &str) -> Result<Vec<Passport>, std::io::Error> {
         }
     }
     vec.push(pass);
-
-    return Ok(vec);
+    Ok(vec)
 }
-
-
 
 // Main function
 fn main() {
@@ -63,11 +161,6 @@ fn main() {
             panic!("Error parsing file: {}", e);
         }
     }
-
-    // Guess: 164 is too low
-    // Guess: 246 is too low
-    // Correct answer was 247
     println!("Total {} passports are valid!", valid_count);
     println!("Total {} passports are invalid!", invalid_count);
-    
 }
--- day4/src/passport.rs
+++ day4/src/passport.rs
@@ -1,17 +1,3 @@
-fn parse_i32(value: &str) -> Option<i32> {
-    let i = value.parse::<i32>();
-    match i {
-        Ok(val) => Some(val),
-        Err(_e) => Some(0)
-    }
-}
-
-fn parse_height(value:&str) -> Option<i32> {
-    let mut s = value.replace("cm", "");
-    s = s.replace("in", "");
-    parse_i32(&s)
-}
-
 pub struct Passport {
     pub byr: Option<i32>,        // (Birth Year)
     pub iyr: Option<i32>,        // (Issue Year)
@@ -45,7 +31,7 @@ impl Passport {
             && self.hcl != None
             && self.ecl != None
             && self.pid != None
-            //&& self.cid != None
+            //&& self.cid != None // Ignore Country ID
     }
 
     pub fn merge(&mut self, pass:&Passport) {
@@ -57,21 +43,6 @@ impl Passport {
         if self.ecl == None { self.ecl = pass.ecl.clone(); }
         if self.pid == None { self.pid = pass.pid; }
         if self.cid == None { self.cid = pass.cid; }
-    }
-
-    pub fn set(&mut self, param: &str) {
-        let mut parts = param.split(":");
-        match parts.next().unwrap() {
-            "byr" => self.byr = parse_i32(parts.next().unwrap()),
-            "iyr" => self.iyr = parse_i32(parts.next().unwrap()),
-            "eyr" => self.eyr = parse_i32(parts.next().unwrap()),
-            "hgt" => self.hgt = parse_height(parts.next().unwrap()),
-            "hcl" => self.hcl = Some(String::from(parts.next().unwrap())),
-            "ecl" => self.ecl = Some(String::from(parts.next().unwrap())),
-            "pid" => self.pid = parse_i32(parts.next().unwrap()),
-            "cid" => self.cid = parse_i32(parts.next().unwrap()),
-            _ => panic!("Unknwon param name!")
-        }
     }
 
     pub fn print_all(&self) {
