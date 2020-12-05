c8f5d7d558ae4e1c526f7940802c5f819caf5c26
Jussi Kallio <jkallio@gmail.com>
2020-12-05 19:19:50 +0200

    Found solution for Part-2 (dirty)

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -20,23 +20,51 @@ fn parse_row_and_seat(input: &str) -> Result<(i32, i32), std::io::Error> {
     Ok((row.0,  col.0))
 }
 
-fn calculate_row_id(row:i32, col:i32) -> i32 {
+fn calculate_seat_id(row:i32, col:i32) -> i32 {
     return row * 8 + col;
+}
+
+fn create_seats_vector() -> Vec<Vec<bool>> {
+    let mut seats = Vec::new();
+    for x in 0..128 {
+        let mut row = Vec::new();
+        for y in 0..8 {
+            row.push(false);
+        }
+        seats.push(row);
+    }
+    return seats;
 }
 
 fn main() {
     let filename = "input.txt";
     if let Ok(vec) = file_reader::read_to_vec(filename) {
-        let mut highest_row_id = 0;
+        
+        let mut seats = create_seats_vector();
+
         for line in vec {
             if let Ok((row, col)) = parse_row_and_seat(&line) {
-                let id = calculate_row_id(row, col);
-                println!("row={}; col={}; id={}", row, col, id);
-                if id > highest_row_id {
-                    highest_row_id = id;
-                }
+                let id = calculate_seat_id(row, col);
+                seats[(row as usize)][(col as usize)] = true;
+                //println!("row={}; col={}; id={}", row, col, id);
             }
         }
-        println!("Highest row id = {}", highest_row_id);
+
+        let mut i = 0;
+        for row in seats {
+            print!("Row-{}: ", i);
+            let mut j = 0;
+            for seat in row {
+                if !seat {
+                    print!{"{}", calculate_seat_id(i, j)}
+                }
+                else {
+                    print!("{} ", seat);
+                }
+                j += 1;
+            }
+            i += 1;
+            println!("");
+        }
     }
 }
