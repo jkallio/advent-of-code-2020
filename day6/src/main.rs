6372fa61ecfb763133f2739e24ab42be031489f7
Jussi Kallio <jkallio@gmail.com>
2020-12-06 16:04:50 +0200

    Cleaned the code and added some comments

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,26 +2,18 @@ mod jk;
 use jk::file_reader;
 use std::collections::HashMap;
 
+// Counts the same, and unique answers for each group and adds them together as a total sum
+// -> Returns the total sum of unique and the same answers of all groups
 fn count_group_answers(input: &Vec<String>) -> (i32, i32) {
     let mut tot_unique_answers:i32 = 0;
     let mut tot_same_answers:i32 = 0;
 
     let mut group_answers = HashMap::new();
     let mut group_size:i32 = 0;
-    for line in input {
-        if line.len() == 0 {
-            let mut same_answers = 0;
-            for (_, v) in group_answers.iter() {
-                if *v as i32 == group_size {
-                    same_answers += 1;
-                }
-            }
-            tot_same_answers += same_answers;
-            tot_unique_answers += group_answers.len() as i32;
-            group_answers = HashMap::new();
-            group_size = 0;
-        }
-        else {
+    for (i, line) in input.iter().enumerate() {
+        
+        // Each line in the input vector represents the answers of one person in a group
+        if line.len() > 0 {
             group_size += 1;
             for c in line.chars() {
                 let mut count = 1;
@@ -31,15 +23,28 @@ fn count_group_answers(input: &Vec<String>) -> (i32, i32) {
                 group_answers.insert(c, count);
             }
         }
-    }
-    let mut same_answers = 0;
-    for (_, v) in group_answers.iter() {
-        if *v as i32 == group_size {
-            same_answers += 1;
+
+        // Groups are separated by blank line. So after blank line encountered increment the total answer count variables.
+        // Also remember to calculate also the last group's answers
+        if line.len() == 0 || i == input.len()-1 {
+
+            // Count the same answers within a group
+            let mut same_answers = 0;
+            for (_, v) in group_answers.iter() {
+                if *v as i32 == group_size {
+                    same_answers += 1;
+                }
+            }
+
+            // Increment the total counters
+            tot_same_answers += same_answers;
+            tot_unique_answers += group_answers.len() as i32;
+            
+            // Reset the Hash Map and the Group size counter (prepare for the next group)
+            group_answers = HashMap::new();
+            group_size = 0;
         }
     }
-    tot_same_answers += same_answers;
-    tot_unique_answers += group_answers.len() as i32;
     return (tot_unique_answers, tot_same_answers);
 }
 
