b33c8ceec2d04ad37fa5a62512f15f6a4e36af4b
Jussi Kallio <jkallio@gmail.com>
2020-12-06 13:30:04 +0200

    Found solution for part-1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,9 +1,31 @@
 mod jk;
 use jk::file_reader;
+use std::collections::HashMap;
 
 fn main() {
     let filename = "input.txt";
-    let Ok(vec) = file_reader::read_to_vec(filename) {
-
+    if let Ok(vec) = file_reader::read_to_vec(filename) {
+        let mut answers = HashMap::new();
+        let mut total_count = 0;
+        for line in vec {
+            if line.len() == 0 {
+                // Clear HashMap for new group
+                total_count += answers.len();
+                println!("Num of answers is {}", answers.len());
+                answers = HashMap::new();
+            }
+            else {
+                for c in line.chars() {
+                    let mut count = 1;
+                    if let Some(current) = answers.get(&c) {
+                        count += current;
+                    }
+                    answers.insert(c, count);  
+                }
+            }
+        }
+        total_count += answers.len(); // Also add the last group answers
+        
+        println!("Total count is {}", total_count);
     }
 }
