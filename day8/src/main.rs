078d209bb0f280861a6d36aa978e6b1632afa06c
Jussi Kallio <jkallio@gmail.com>
2020-12-08 19:54:59 +0200

    Added basic file reading

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+const cmd_list:[&'static str; 3] = ["acc", "jmp", "nop"];
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input.txt";
+
+    let file = File::open(filename);
+    let mut tree_count = 0;
+    let mut pos = 0;
+    let br = BufReader::new(file.unwrap());
+    for (index, buf) in br.lines().enumerate() {  
+        println!("{}", buf);
+    }
 }
