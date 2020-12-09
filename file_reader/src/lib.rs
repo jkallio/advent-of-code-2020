75f99821676806edf2f3342ab994512cfb59fa8f
Jussi Kallio <jkallio@gmail.com>
2020-12-09 22:42:53 +0200

    Took rustfmt changes

--- file_reader/src/lib.rs
+++ file_reader/src/lib.rs
@@ -2,9 +2,8 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind};
 
-
 /// Reads given file line by line into a vector of Strings.
-/// 
+///
 /// # Examples
 /// ```
 /// let file_path = "test_input_i32.txt";
@@ -27,7 +26,7 @@ pub fn read_to_string_vec(path: &str) -> Result<Vec<String>, std::io::Error> {
 }
 
 /// Reads given file line by line into a vector of 32-bit integers.
-/// 
+///
 /// # Examples
 /// ```
 /// let file_path = "test_input_i32.txt";
@@ -45,7 +44,9 @@ pub fn read_to_i32_vec(path: &str) -> Result<Vec<i32>, std::io::Error> {
     let mut vec = Vec::new();
     for line in br.lines() {
         let line = line?;
-        let n:Result<i32, std::io::Error>= line.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e));
+        let n: Result<i32, std::io::Error> = line
+            .parse()
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e));
         vec.push(n?);
     }
     Ok(vec)
