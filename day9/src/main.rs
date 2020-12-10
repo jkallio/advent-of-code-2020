f6b04fdf35cd839af77da48ca5ef83c9e10e969a
Jussi Kallio <jkallio@gmail.com>
2020-12-10 17:33:54 +0200

    Added fmt and clippy suggestions

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,27 +1,23 @@
-use file_reader;
-
 /// Returns `true` if given vector contains at least one pair of
 /// addends that sum up to the value found from the last position
 /// of the input vector.
-/// 
+///
 /// # Arguments
 /// * `vec` input vector that contains the values where to search the addends.
-/// 
+///
 /// # Examples
 /// let vec = vec![35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576];
-/// 
-fn find_addend_pair(vec:Vec<i64>, sum_target:i64) -> bool {
+///
+fn find_addend_pair(vec: Vec<i64>, sum_target: i64) -> bool {
     let max_index = vec.len();
     for (i, a) in vec.iter().enumerate() {
         for (j, b) in vec.iter().enumerate() {
-            if i < max_index && j < max_index {
-                if a + b == sum_target {
-                    return true;
-                }
+            if i < max_index && j < max_index && a + b == sum_target {
+                return true;
             }
         }
     }
-    return false;
+    false
 }
 
 /// Main function
@@ -31,7 +27,7 @@ fn main() {
     match vec {
         Ok(vec) => {
             for slice in vec.windows(26) {
-                if find_addend_pair((&slice[..25]).to_vec(), slice[25]) == false {
+                if !find_addend_pair((&slice[..25]).to_vec(), slice[25]) {
                     println!("Not found! value is {}", slice.last().unwrap());
                 }
             }
