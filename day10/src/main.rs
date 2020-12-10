b28c5c1c84ca98ec7ad6b295e08225f7f8ebc861
Jussi Kallio <jkallio@gmail.com>
2020-12-10 23:54:31 +0200

    Found solution for part-1

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let input = "input.txt"; 
+    let mut vec = file_reader::read_to_i64_vec(input).unwrap();
+    vec.sort();
+    let mut diff_1 = 1; // Starting from 1 because first outlet is 0 and first adapter is 1
+    let mut diff_3 = 1; // Starting from 3 because internal adapter is 3 higher than highest adapter in the bag
+    for adapters in vec.windows(2) {
+        let diff = adapters[1] - adapters[0];
+        match diff {
+            1 => { diff_1 += 1; }
+            3 => { diff_3 += 1; }
+            _ => { panic!(""); }
+        }
+        //println!("{} --> ({}-{})", adapters[1], diff_1, diff_3);
+    }
+    println!("{}", diff_1 * diff_3);
 }
