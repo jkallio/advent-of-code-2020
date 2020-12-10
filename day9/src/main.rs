314b8f2ce4c72220c9b4008aac6fbdb57534b970
Jussi Kallio <jkallio@gmail.com>
2020-12-10 17:28:22 +0200

    Cleaned the implementation. Added comments

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,30 +1,38 @@
 use file_reader;
 
-fn count_pairs(vec:Vec<i64>) -> i64 {
-    let mut count:i64 = 0;
-    let target_value = vec.last().unwrap();
-    let max_index = vec.len() - 1;
+/// Returns `true` if given vector contains at least one pair of
+/// addends that sum up to the value found from the last position
+/// of the input vector.
+/// 
+/// # Arguments
+/// * `vec` input vector that contains the values where to search the addends.
+/// 
+/// # Examples
+/// let vec = vec![35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576];
+/// 
+fn find_addend_pair(vec:Vec<i64>, sum_target:i64) -> bool {
+    let max_index = vec.len();
     for (i, a) in vec.iter().enumerate() {
         for (j, b) in vec.iter().enumerate() {
             if i < max_index && j < max_index {
-                if a + b == *target_value {
-                    count += 1;
+                if a + b == sum_target {
+                    return true;
                 }
             }
         }
     }
-    return count;
+    return false;
 }
 
+/// Main function
 fn main() {
     let input = "input.txt";
     let vec = file_reader::read_to_i64_vec(input);
     match vec {
         Ok(vec) => {
             for slice in vec.windows(26) {
-                if count_pairs(slice.to_vec()) == 0 {
+                if find_addend_pair((&slice[..25]).to_vec(), slice[25]) == false {
                     println!("Not found! value is {}", slice.last().unwrap());
-
                 }
             }
         }
