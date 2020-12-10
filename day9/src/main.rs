d0d83942dbd41c60c38536dd874f04207268f5ba
Jussi Kallio <jkallio@gmail.com>
2020-12-10 22:41:32 +0200

    Cleaned some code

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -3,13 +3,6 @@
 /// # Arguments
 /// * `vec` input from where to search the addends.
 /// * `sum_target` target value for the sum.
-///
-/// # Examples
-/// ```
-/// let vec = vec![35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576];
-/// let sum_target = 403; // = 95 + 309
-/// assert_eq!(find_addend_pair(vec, sum_target), true);
-/// ```
 fn find_addend_pair(vec: &Vec<i64>, sum_target: i64) -> bool {
     let max_index = vec.len();
     for (i, a) in vec.iter().enumerate() {
@@ -22,7 +15,42 @@ fn find_addend_pair(vec: &Vec<i64>, sum_target: i64) -> bool {
     false
 }
 
+/// Returns first i64 in `vec` that does not follow the rule where each number must be the sum of
+/// any two earlier number within a window of 25.
+/// 
+/// # Arguments
+/// * `vec` input vector containing the number pool
+fn find_first_invalid_number(vec: &Vec<i64>) -> Option<i64> {
+    for slice in vec.windows(26) {        
+        if !find_addend_pair(&slice.to_vec(), slice[25]) {
+            return Some(slice[25]);
+        }
+    }
+    None
+}
 
+/// Returns the "encryption weakness" which is the sum of smallest and largest values in the
+/// window that sums up to `target_sum`
+/// 
+/// # Arguments
+/// `vec` input vector containing the number pool
+/// `target_sum` target sum that we're trying to find
+fn find_encryption_weakness(vec: &Vec<i64>, target_sum:i64) -> Option<i64> {
+    let mut window_size = 2;
+    loop {
+        for slice in vec.windows(window_size) {
+            let sum:i64 = slice.iter().sum();
+            if sum == target_sum {
+                return Some(slice.iter().min().unwrap() + slice.iter().max().unwrap());
+            }
+        }
+        
+        window_size += 1;
+        if window_size >= vec.len() {
+            return None;
+        }
+    }
+}
 
 /// Main function
 fn main() {
@@ -30,37 +58,13 @@ fn main() {
     let vec = file_reader::read_to_i64_vec(input);
     match vec {
         Ok(vec) => {
-            // Part-1 --> 
-            let mut invalid_number:i64 = 0;
-            for slice in vec.windows(26) {
-                let sum_target:i64 = *slice.last().unwrap();
-                if !find_addend_pair(&(&slice[..25]).to_vec(), sum_target) {
-                    invalid_number = sum_target;
-                    break;
-                }
-            }
+            // Part-1:
+            let invalid_number = find_first_invalid_number(&vec).unwrap();
             println!("First value to not to follow the rule is {}", invalid_number);
 
-            // Part-2 --> 
-            let mut window_size = 2;
-            'outer: loop {
-                println!("Window size = {}", window_size);
-                if window_size < vec.len() {
-                    for slice in vec.windows(window_size) {
-                        let sum:i64 = slice.iter().sum();
-                        if sum == invalid_number {
-                            let encryption_weakness = slice.iter().min().unwrap() + slice.iter().max().unwrap();
-                            println!("Encryption weakness is {}", encryption_weakness);
-                            break 'outer;
-                        }
-                    }
-                }
-                else {
-                    println!("ERR! Window size exceeds vector size");
-                    break;
-                }
-                window_size += 1;
-            }
+            // Part-2:
+            let encryption_weakness = find_encryption_weakness(&vec, invalid_number).unwrap();
+            println!("Encryption weakness is {}", encryption_weakness);
         }
         Err(e) => {
             println!("{}", e);
