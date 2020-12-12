6dab91d20a78c599be70d50daa78d145af2ed210
Jussi Kallio <jkallio@gmail.com>
2020-12-12 12:11:10 +0200

    Added input file

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let input_file = "input.txt";
+    let input = file_utils::read_to_string_vec(input_file).unwrap();
+    for line in input {
+        println!("{}", line);
+    }
 }
