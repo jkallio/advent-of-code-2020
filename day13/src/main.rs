c78c8151ec3d69fd71699d4a8fed0d9e3e6e5d46
Jussi Kallio <jkallio@gmail.com>
2020-12-13 17:55:45 +0200

    Moved part-1 solution to its own function

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,37 +1,54 @@
 use regex::Regex;
 
-fn parse_bus_schedule(input: &[String]) -> (i32, Vec<i32>) {
+// Returns the earliest timestamp for departure with a list of bus ids
+// Each bus also contains the delta time from t=0
+fn parse_bus_schedule(input: &[String]) -> (i32, Vec<(i32, i32)>) {
     if input.len() == 2 {
-        let mut schedule: i32 = 0;
-        let mut buses = Vec::new();
+        let mut departure: i32 = 0;
+        let mut bus_list = Vec::new();
+
+        // 1st line contains the earliest timestamp for departure
         if let Ok(i) = input[0].parse() {
-            schedule = i;
+            departure = i;
         }
+
+        // Second line consists of bus id (also doubles as round-trip time) 
+        let mut t = 0;
         for x in input[1].split(',') {
             let re = Regex::new(r"([0-9])+").unwrap();
             if re.is_match(x) {
                 if let Ok(i) = x.parse() {
-                    buses.push(i);
+                    bus_list.push((i, t));
                 }
             }
+            t += 1;
         }
-        return (schedule, buses);
+        return (departure, bus_list);
     }
     panic!("Invalid input");
 }
 
+// Returns the ID of the earliest bus you can take multiplied with the number of
+// minutes you'll need to wait for the bus.
+fn get_answer_for_part1(info: (i32, Vec<(i32, i32)>)) -> i32 {
+    let departure = info.0;
+    let mut closest_to_departure = (0, i32::MAX); // bus_id & delta from departure time
+
+    for bus in info.1 {
+        let modulo = departure % bus.0;
+        let diff = (modulo - bus.0).abs();
+        if diff < closest_to_departure.1 {
+            closest_to_departure.1 = diff;
+            closest_to_departure.0 = bus.0;
+        }
+    }
+    closest_to_departure.0 * closest_to_departure.1
+}
+
+
 fn main() {
     if let Ok(input) = file_utils::read_to_string_vec("test_input.txt") {
         let info = parse_bus_schedule(&input);
-        let mut smallest = (0, i32::MAX);
-        for bus in info.1 {
-            let modulo = info.0 % bus;
-            let diff = (modulo - bus).abs();
-            if diff < smallest.1 {
-                smallest.1 = diff;
-                smallest.0 = bus;
-            }
-        }
-        println!("Answer for part1 = {}", smallest.0 * smallest.1);
+        println!("Answer for part-1 is {}", get_answer_for_part1(info));        
     }
 }
