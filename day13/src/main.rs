f743f01676060de2e74d0b8fea439155f700b8f5
Jussi Kallio <jkallio@gmail.com>
2020-12-14 00:14:29 +0200

    Cleaned the output

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -50,40 +50,48 @@ fn main() {
         id: i64,
         t: i64,
         timestamp:i64,
+        addon:i64,        
     }
 
     if let Ok(input) = file_utils::read_to_string_vec("test_input1.txt") {
         let info = parse_bus_schedule(&input);
-        //let part1 = get_answer_for_part1(&info);
-        //println!("Answer for part-1 is {}", part1);
+        
+        // Part-1
+        let part1 = get_answer_for_part1(&info);
+        println!("Answer for part-1 is {}", part1);
 
+        // Part-2
         let mut buses = Vec::new();
         for bus in info.1 {
             let bus = Bus { 
                 id:bus.0 as i64,
                 t:bus.1 as i64,
-                timestamp:bus.0 as i64
+                timestamp:bus.0 as i64,
+                addon:(bus.0 - bus.1) as i64,
             };
             buses.push(bus);
         }
         buses.sort_by_key(|k| k.id);
 
         let mut target_t0 = 0;
+        let mut biggest_match = 1;
         loop {
             let mut matches = 0;
             for bus in buses.iter_mut() {
                 let target = target_t0 + bus.t;
                 while bus.timestamp < target {
-                    bus.timestamp += &bus.id;
+                    bus.timestamp += bus.id;
                 }
                 if bus.timestamp > target {
-                    target_t0 = bus.timestamp - bus.t; 
+                    target_t0 = bus.timestamp - bus.t;
                     break;
                 }
                 else if bus.timestamp == target {
                     matches += 1;
-                    if matches > 2 {
-                        println!("Matches={}; target={}", matches, target_t0);
+                    if matches > biggest_match {
+                        biggest_match = matches;
+                        bus.addon = target_t0;
+                        println!("Matches = {}; target_t0 = {};", matches, target_t0);
                     }
                 }
             }  
