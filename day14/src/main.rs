6eb8e4bee55f833182a49e74d6702dd45e1f7f7d
Jussi Kallio <jkallio@gmail.com>
2020-12-16 00:14:36 +0200

    Found solution for part-2 (Still too many iterations

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -55,34 +55,50 @@ fn parse_mask(line: &str) -> (i64, i64, i64) {
     (zero_mask, one_mask, floating_mask)
 }
 
-fn recursive_mem_write(mut mem:&mut HashMap<i64, i64>, addr:i64, value:i64, floating_mask:i64, debug_it:i64) {
+fn recursive_get_address_list(mut mem:&mut HashMap<i64, i64>, addr:i64, value:i64, floating_mask:i64) -> Vec<i64> {
+    let mut vec = Vec::<i64>::new();
     for i in (0..36).rev() {
         let floater = 1 << (35 - i);
         if floating_mask & floater != 0 {
             
             let a0 = addr & !floater;
-            mem.insert(a0, value);
-            println!("Write mem[{}] = {} (addr={:#b}; fm={:#b}; debug={})", a0, value, a0, floating_mask, debug_it);
-            recursive_mem_write(&mut mem, a0, value, floating_mask & !floater, debug_it+1);
+            let mut v0 = recursive_get_address_list(&mut mem, a0, value, floating_mask & !floater);
+            if !v0.contains(&a0) {
+                v0.push(a0);
+            }
+            vec.append(&mut v0);
 
             let a1 = addr | floater;
-            mem.insert(addr, value);
-            println!("Write mem[{}] = {} (addr={:#b}; fm={:#b}; debug={})", addr, value, addr, floating_mask, debug_it);
-            recursive_mem_write(&mut mem, a1, value, floating_mask & !floater, debug_it+1);
+            let mut v1 = recursive_get_address_list(&mut mem, a1, value, floating_mask & !floater);
+            if !v1.contains(&a1) {
+                v1.push(a1);
+            }
+            vec.append(&mut v1);
+
             break;
         }
 
     }
+    return vec;
 }
 
 fn main() {
-    if let Ok(input) = file_utils::read_to_string_vec("test_input.txt") {
+    if let Ok(input) = file_utils::read_to_string_vec("input.txt") {
         let mut masks: (i64, i64, i64) = (0, 0, 0);
         let mut mem = HashMap::<i64, i64>::new();
         for line in input {
             if line.starts_with("mem") {
                 let m = parse_mem_instruction(&line);
-                recursive_mem_write(&mut mem, m.0 | masks.1, m.1, masks.2, 0);
+
+                let addr = m.0 | masks.1;
+                let value = m.1;
+
+                let vec = recursive_get_address_list(&mut mem, addr, value, masks.2);
+                for it in vec {
+                    println!("Write mem[{}] = {}", it, value);
+                    mem.insert(it, value);
+                }
+
             } else if line.starts_with("mask") {
                 masks = parse_mask(&line);
             }
@@ -96,7 +112,5 @@ fn main() {
         }
         println!("------------------------");
         println!("Total sum of the values in memory is {}", tot_sum);
-
-        // 2240300118827 is too low
     }
 }
