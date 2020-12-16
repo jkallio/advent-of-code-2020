fd776217170b7475e27410b7099960ed44affb0e
Jussi Kallio <jkallio@gmail.com>
2020-12-16 19:34:43 +0200

    Found solution for part-1

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut history: Vec<i32> = vec![14, 1, 17, 0, 3, 20];
+    for turn in history.len() + 1..=2020 {
+        let mut age: i32 = 0;
+        if let Some(test) = history.last() {
+            for (i, v) in history.iter().rev().enumerate() {
+                if i > 0 && *v == *test {
+                    age = i as i32;
+                    break;
+                }
+            }
+        }
+        println!("#{} = {}", turn, age);
+        history.push(age);
+    }
 }
