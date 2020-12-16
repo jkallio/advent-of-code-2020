a0b0dce373641f022d5d6695677c4f9ec7a6e44e
Jussi Kallio <jkallio@gmail.com>
2020-12-16 22:01:54 +0200

    Found solution for part2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,16 +1,26 @@
+use std::collections::HashMap;
+
 fn main() {
-    let mut history: Vec<i32> = vec![14, 1, 17, 0, 3, 20];
-    for turn in history.len() + 1..=2020 {
-        let mut age: i32 = 0;
-        if let Some(test) = history.last() {
-            for (i, v) in history.iter().rev().enumerate() {
-                if i > 0 && *v == *test {
-                    age = i as i32;
-                    break;
-                }
-            }
+    // Initialize the game
+    let input: Vec<i32> = vec![14, 1, 17, 0, 3, 20];
+    let mut history = HashMap::<i32, i32>::new();
+    let mut last_spoken = 0;
+    for (i, n) in input.iter().enumerate() {
+        if n == input.last().unwrap() {
+            last_spoken = *n;
+        } else {
+            history.insert(*n, 1 + (i as i32));
         }
-        println!("#{} = {}", turn, age);
-        history.push(age);
     }
+
+    // Play the game
+    for turn in (input.len() + 1)..=30000000 {
+        let mut age: i32 = 0;
+        if let Some(value) = history.get(&last_spoken) {
+            age = turn as i32 - *value - 1;
+        }
+        history.insert(last_spoken, turn as i32 - 1);
+        last_spoken = age;
+    }
+    println!("Last spoken number is {}", last_spoken);
 }
