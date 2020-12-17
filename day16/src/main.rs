5e588f9c555e926aef1f314b4debef77cac97438
Jussi Kallio <jkallio@gmail.com>
2020-12-17 22:57:16 +0200

    Found solution for part1

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -9,9 +9,16 @@ struct ValueRange {
     max:i32
 } 
 
+impl ValueRange {
+    fn value_in_range(&self, v:i32) -> bool {
+        v >= self.min && v <= self.max
+    }
+}
+
 fn parse_input_file(
     path: &str,
-    ranges: &mut HashMap::<String, Vec<ValueRange>>
+    ranges: &mut HashMap::<String, Vec<ValueRange>>,
+    tickets: &mut Vec<Vec<i32>>
 ) -> Result<bool, std::io::Error> {
     
     let file = File::open(path)?;
@@ -50,7 +57,17 @@ fn parse_input_file(
 
                     }
                     2 => {  // Nearby Ticket
-
+                        if line.contains(",") {
+                            let parts_iter = line.split(",");
+                            let mut values = Vec::<i32>::new();
+                            for value in parts_iter {
+                                let n: Result<i32, std::io::Error> = value
+                                    .parse()
+                                    .map_err(|e| Error::new(ErrorKind::InvalidData, e));
+                                values.push(n?);
+                            }
+                            tickets.push(values);
+                        }
                     }
                     _ => {
                         panic!("Invalid Section");
@@ -62,11 +79,40 @@ fn parse_input_file(
     Ok(true)
 }
 
-fn main() {
-    let mut ranges = HashMap::<String, Vec<ValueRange>>::new();
+fn sum_invalid(
+    ranges: &HashMap::<String, Vec<ValueRange>>,
+    ticket: &Vec<i32>
+) -> i64 {
+    let mut sum:i64 = 0;
+    
+    for value in ticket {
+        let mut is_valid = false;
+        'outer: for category in ranges {    
+            for rng in category.1 {
+                if rng.value_in_range(*value) {
+                    is_valid = true;
+                    break 'outer;
+                }
+            }
+        }
+        if !is_valid {
+            println!("Invalid value found = {}", value);
+            sum += *value as i64;
+        }
+    }
+    sum
+}
 
-    parse_input_file("input.txt", &mut ranges);
-    for (k, v) in ranges {
-        println!("{} = {:?}", k, v);
+fn main() {
+    let input = "input.txt";
+    let mut ranges = HashMap::<String, Vec<ValueRange>>::new();
+    let mut tickets = Vec::<Vec<i32>>::new();
+    
+    if let Ok(_) = parse_input_file(input, &mut ranges, &mut tickets) {
+        let mut tot_sum:i64 = 0;
+        for ticket in tickets {
+            tot_sum += sum_invalid(&ranges, &ticket);
+        }
+        println!("Total sum of invalid values is {}", tot_sum);
     }
 }
