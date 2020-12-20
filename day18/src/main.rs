51f915359cba9075327aec1bf288116bfe4d6883
Jussi Kallio <jkallio@gmail.com>
2020-12-20 12:15:58 +0200

    Accpeted rustfmt

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,15 +1,16 @@
-use std::fs::File;
-use std::io::{ BufReader, BufRead };
 use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 // Solves the equations inside parentheses and returns the given equation in form where all parentheses have been resolved
 // Call itself recursively (each recursion solves one equation inside parentheses)
 fn recursive_simplify_parentheses(s: &str) -> String {
-    let re = Regex::new(r"\(([0-9]+(\s)*(\*|\+)(\s)*[0-9]+(\s)*((\*|\+)(\s)*[0-9]+(\s)*)*\))").unwrap();
+    let re =
+        Regex::new(r"\(([0-9]+(\s)*(\*|\+)(\s)*[0-9]+(\s)*((\*|\+)(\s)*[0-9]+(\s)*)*\))").unwrap();
     let mut equation = String::from(s);
     println!("{}", equation);
     if let Some(m) = re.find(&s) {
-        let solved = solve_equation(&s[m.start()+1..m.end()-1]);
+        let solved = solve_equation(&s[m.start() + 1..m.end() - 1]);
         equation.replace_range(m.start()..m.end(), &solved.to_string());
         println!("{}", equation);
         equation = recursive_simplify_parentheses(&equation);
@@ -19,7 +20,7 @@ fn recursive_simplify_parentheses(s: &str) -> String {
 
 // Solves the equation by first simplifying parentheses and then adding/multiplying values from left to right
 // Note that in this problem '+' and '*' have the same precedence (unlike in real Math)
-fn solve_equation(s:&str) -> i64 {
+fn solve_equation(s: &str) -> i64 {
     enum Sign {
         MULTIPLICATION,
         ADDITION,
@@ -27,17 +28,25 @@ fn solve_equation(s:&str) -> i64 {
     let mut equation = String::from(s);
     equation = recursive_simplify_parentheses(&equation);
     let mut sign = Sign::ADDITION;
-    let mut result:i64 = 0;
+    let mut result: i64 = 0;
     for p in equation.split(' ') {
         match p {
-            "*" => { sign = Sign::MULTIPLICATION; }
-            "+" => { sign = Sign::ADDITION; }
-            _ => { 
+            "*" => {
+                sign = Sign::MULTIPLICATION;
+            }
+            "+" => {
+                sign = Sign::ADDITION;
+            }
+            _ => {
                 let operand = p.parse::<i64>().unwrap();
 
                 match sign {
-                    Sign::ADDITION => { result += operand; }
-                    Sign::MULTIPLICATION => { result *= operand; }
+                    Sign::ADDITION => {
+                        result += operand;
+                    }
+                    Sign::MULTIPLICATION => {
+                        result *= operand;
+                    }
                 }
             }
         }
@@ -51,7 +60,7 @@ fn main() {
     let input = "input.txt";
     let file = File::open(input).unwrap();
     let br = BufReader::new(file);
-        
+
     let mut results = vec![];
     for line in br.lines() {
         let line = line.unwrap();
