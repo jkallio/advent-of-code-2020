f20f0bbd3d1c80c0de1a638305e1dc5f6c6978cd
Jussi Kallio <jkallio@gmail.com>
2020-12-20 11:49:17 +0200

    Found solution for part1

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,34 +1,46 @@
 use std::fs::File;
 use std::io::{ BufReader, BufRead };
 use regex::Regex;
-//use regex::Matches;
 
-fn solve(s:&str) -> String {
-    let mut solved = String::from(s);
-    println!("{}", solved);
-    solved.retain(|c| c != '(' && c != ')');
-
-    let parts = solved.split(' ');
-
-    for p in parts {
-        println!("{}", p);
+fn solve(s:&str) -> i64 {
+    enum Sign {
+        MULTIPLICATION,
+        ADDITION,
     }
+    let equation = String::from(s);
+    /*
+    while equation.contains('(') {
+        simplify_parentheses(&mut equation);
+    }
+    //equation.retain(|c| c != '(' && c != ')');
+    */
+    let mut sign = Sign::ADDITION;
+    let mut result:i64 = 0;
+    for p in equation.split(' ') {
+        match p {
+            "*" => { sign = Sign::MULTIPLICATION; }
+            "+" => { sign = Sign::ADDITION; }
+            _ => { 
+                let operand = p.parse::<i64>().unwrap();
 
-    //let re = Regex::new(r"\*|\+").unwrap();
-    
-
-    println!("{}", solved);
-
-    solved
+                match sign {
+                    Sign::ADDITION => { result += operand; }
+                    Sign::MULTIPLICATION => { result *= operand; }
+                }
+            }
+        }
+    }
+    println!("{} = {}", equation, result);
+    result
 }
 
 fn simplify_parentheses(s: &mut String) -> bool {
     
     let searh_str = s.clone();
-    let re = Regex::new(r"\(([0-9](\s)*(\*|\+)(\s)*[0-9](\s)*((\*|\+)(\s)*[0-9]*(\s)*)*\))").unwrap();
+    let re = Regex::new(r"\(([0-9]+(\s)*(\*|\+)(\s)*[0-9]+(\s)*((\*|\+)(\s)*[0-9]+(\s)*)*\))").unwrap();
     if let Some(m) = re.find(&searh_str) {
-        let solved = solve(&s[m.start()..m.end()]);
-        s.replace_range(m.start()..m.end(), &solved);
+        let solved = solve(&s[m.start()+1..m.end()-1]);
+        s.replace_range(m.start()..m.end(), &solved.to_string());
         return simplify_parentheses(s);
     }
     false
@@ -39,12 +51,23 @@ fn main() {
     let file = File::open(input).unwrap();
     let br = BufReader::new(file);
         
-    //let results = vec![];
+    let mut results = vec![];
     for line in br.lines() {
         let mut line = line.unwrap();
         println!("{}", line);
-        //line.retain(|c| !c.is_whitespace());
-
-        simplify_parentheses(&mut line);
+        loop {
+            if simplify_parentheses(&mut line) == false {
+                let result:i64 = solve(&line);
+                results.push(result);
+                break;
+            }
+        }
+        println!("----------------------------------");
     }
+
+    let mut answer = 0;
+    for val in results {
+        answer += val;
+    }
+    println!("Final answer is {}", answer);
 }
