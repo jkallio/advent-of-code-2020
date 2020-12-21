3d71c9919421d84369d12526ef0c064cb6f8618e
Jussi Kallio <jkallio@gmail.com>
2020-12-21 16:49:13 +0200

    Added XY Hashmap

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,19 +1,28 @@
 use std::fs::File;
 use std::io::{ BufRead, BufReader };
 use regex::Regex;
+use std::collections::HashMap;
 
 type TileMap = Vec<Tile>;
 
+#[derive(Hash, PartialEq, Eq, Clone)]
+struct XY {
+    x: i32,
+    y: i32
+}
+
 struct Tile {
     id: i32,
-    lines: Vec<String>
+    pixels: HashMap<XY, bool>,
+    borders: Vec<i32>,
 }
 
 impl Tile {
     fn new() -> Tile {
         Tile {
             id: -1,
-            lines: vec![]
+            pixels: HashMap::<XY, bool>::new(),
+            borders: vec![],
         }
     }
 }
@@ -25,10 +34,15 @@ fn parse_input_file(path: &str) -> TileMap {
     
     let mut tiles:Vec<Tile> = vec![];
     let mut tile = Tile::new();
-    for it in br.lines() {
-        let line = it.unwrap();
+
+    let mut y = 0;
+    for line in br.lines() {
+        let line = line.unwrap();
+        
         if !line.is_empty() {
+            // Prepare for new Tile
             if line.contains("Tile") {
+                y = 0;
                 if tile.id > 0 {
                     tiles.push(tile);
                 }
@@ -42,7 +56,11 @@ fn parse_input_file(path: &str) -> TileMap {
                 }
             }
             else {
-                tile.lines.push(line);
+                for (x, c) in line.chars().enumerate() {
+                    let xy = XY { x: x as i32, y: y };
+                    tile.pixels.insert(xy, c == '#');
+                }
+                y += 1;
             }
         }
     }
@@ -51,15 +69,33 @@ fn parse_input_file(path: &str) -> TileMap {
     tiles
 }
 
+fn calculate_border_identifiers(tiles: &mut TileMap) {
+    for (i, tile) in tiles.iter().enumerate() {
+        
+    }
+}
+
+
 fn main() {
     let input = "test_input.txt";
 
-    let tiles = parse_input_file(input);
+    let mut tiles = parse_input_file(input);
+    calculate_border_identifiers(&mut tiles);
+
     for tile in tiles {
         println!("{}", tile.id);
-        for line in tile.lines {
-            println!("{}", line);
+
+        for y in 0..10 {
+            for x in 0..10 {
+                let mut c = '.';
+                if *tile.pixels.get(&XY {x,y}).unwrap() {
+                    c = '#';
+                }
+                print!("{}", c);
+            }
+            println!("");
         }
+        println!("");
     }
 
 
